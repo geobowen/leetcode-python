c95c8a3604c67a697411482f16a5383f92f9a5a5
geobowen <petergao25@126.com>
2024-09-24 17:29:43 +0800

    Update 191_Number_of_1_Bits.py

--- 191_Number_of_1_Bits.py
+++ 191_Number_of_1_Bits.py
@@ -1,4 +1,27 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
 
+        # method 1
+        # n&(n-1) will change the righmost "1" bit to "0" bit
+        # so we can count the "1" bit by repeating the iteration until n becomes 0
+
+        res = 0
+        while n:
+            n &= n-1
+            res += 1
+
+        return res
+
+        '''
+        # method 2
+        # if n & 1 = 1 then the last bit of n is 1, then right shift n
+
+        res = 0
+        while n:
+            res += n & 1
+            n >>= 1
+        return res
+
+        # method 3
         return bin(n).count('1')
+        '''
